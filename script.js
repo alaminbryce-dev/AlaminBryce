ae17dce928c98c5aa2b5f428da1df7119c6abb95
alaminbryce-dev <alaminbryce@gmail.com>
2025-10-24 05:45:42 +0700

    Update script.js

--- script.js
+++ script.js
@@ -123,7 +123,7 @@ document.addEventListener('DOMContentLoaded', renderRanking);
  * - mode: 'count' or 'money'
  */
 const milestoneConfig = {
-  current: 160,                       // ← change this anytime
+  current: 144,                       // ← change this anytime
   unit: 'monthly earning',                   // e.g., 'patrons' or 'USD' or 'IDR'
   mode: 'money',                     // 'count' or 'money'
   milestones: [

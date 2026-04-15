ba423d93a96b7529924414ed51891b30e42d4d83
alaminbryce-dev <alaminbryce@gmail.com>
2026-04-15 21:15:59 +0700

    Update contributor amounts and add new contributors

--- script.js
+++ script.js
@@ -27,13 +27,13 @@ shareBtn.addEventListener('click', async () => {
  * amount: number (USD or any currency symbol you like in formatMoney)
  */
 const contributors = [
-  { name: "Drew", amount: 91 },
-  { name: "Zitronen", amount: 90 },
-  { name: "isaidgetlost", amount: 90 },
-  { name: "Akishira", amount: 89 },
-  { name: "Yam", amount: 78 },
-  { name: "hdctbpal", amount: 65 },
-  { name: "Uiai Uea", amount: 35 }
+  { name: "Drew", amount: 143 },
+  { name: "Zitronen", amount: 120 },
+  { name: "Jimpikins", amount: 54 },
+  { name: "Akishira", amount: 140 },
+  { name: "DerBlutKaiser", amount: 65 },
+  { name: "hdctbpal", amount: 104 },
+  { name: "Richard Duffus", amount: 75 }
 ];
 
 function formatMoney(n){ 

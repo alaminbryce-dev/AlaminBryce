0f8ba749c7376673a8a3a231db89d9b04b13f16d
alaminbryce-dev <alaminbryce@gmail.com>
2026-06-22 01:59:15 +0700

    Update current milestone value from 237 to 236

--- script.js
+++ script.js
@@ -123,7 +123,7 @@ document.addEventListener('DOMContentLoaded', renderRanking);
  * - mode: 'count' or 'money'
  */
 const milestoneConfig = {
-  current: 237,                       // ← change this anytime
+  current: 236,                       // ← change this anytime
   unit: 'monthly earning',                   // e.g., 'patrons' or 'USD' or 'IDR'
   mode: 'money',                     // 'count' or 'money'
   milestones: [
